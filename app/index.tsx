6f170b7dbe60e06c95128a453cd5e031e222e97f
akeats97 <akeats97@gmail.com>
2025-05-10 08:08:21 -0400

    Fix circular import and redirect to tabs navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Redirect } from 'expo-router';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href="/(tabs)" />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#800080', // Purple color
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFFFFF', // White color
-    fontSize: 24,
-  },
-});
